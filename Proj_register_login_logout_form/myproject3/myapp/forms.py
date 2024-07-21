0d413171a106ede678a29c202166ba4bd5e3c901
Naveennml <naveenml1992@gmail.com>
2024-07-21 18:59:48 +0530

    Update forms.py

--- Proj_register_login_logout_form/myproject3/myapp/forms.py
+++ Proj_register_login_logout_form/myproject3/myapp/forms.py
@@ -1,24 +1,25 @@
+# myapp/forms.py
 from django import forms
 from django.contrib.auth.models import User
 from .models import UserProfileInfo
 
 class UserForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput())
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
-    class Meta():
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
         model = User
-        fields = ('username','email','password1', 'password2')
+        fields = ('username', 'email', 'password')
 
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
 
-
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
 
 class UserProfileInfoForm(forms.ModelForm):
-    class Meta():
+    class Meta:
         model = UserProfileInfo
-        fields = ('portfolio_site','profile_pic')
+        fields = ('portfolio_site', 'profile_pic')
